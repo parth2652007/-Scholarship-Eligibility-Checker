0e5792f02418716998bc434f2d575cea0c477cd0
parth2652007 <parthsakariya911@gmail.com>
2026-01-28 17:17:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,34 @@ let scholarships = [];
 
 // CONNECT JSON FILE
 fetch("scholarships.json")
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     scholarships = data;
     console.log("JSON connected:", scholarships);
   })
-  .catch(error => console.error("Error loading JSON:", error));
+  .catch((error) => console.error("Error loading JSON:", error));
 
 function checkScholarship() {
   const category = document.getElementById("category").value;
+  const gender=document.getElementById("gender").value;
   const income = Number(document.getElementById("income").value);
   const education = document.getElementById("education").value;
   const state = document.getElementById("state").value.toLowerCase();
 
   let html = "";
 
-  scholarships.forEach(s => {
+  scholarships.forEach((s) => {
     if (
       (category === "" || s.Category === category || s.Category === "All") &&
       (education === "" || s.Education === education) &&
-      income <= s.Income &&
+      ((
+  gender === "" ||
+  s.Gender === "All" ||
+  s.Gender === gender
+)
+&&
       (state === "" || s.State === "All" || s.State.toLowerCase() === state)
+      )
     ) {
       html += `
         <div class="scholarship-card">
@@ -37,3 +44,4 @@ function checkScholarship() {
   document.getElementById("results").innerHTML =
     html || `<p class="no-result">No scholarships found</p>`;
 }
+
